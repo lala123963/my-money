d11e06f54c7d07a047b2cfa529b6ada63f781619
lgq-ll <57555508+lgq-ll@users.noreply.github.com>
2022-09-05 10:22:18 +0800

    Update dbtools.sh

--- jd/dbtools.sh
+++ jd/dbtools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## 版本号
-Ver="Build 20220323-001-Alpha"
+Ver="Build 20220831-001-Alpha"
 
 ## 导入通用变量与函数
 dir_shell=/ql/shell
@@ -10,12 +10,12 @@ dir_shell=/ql/shell
 
 # 定义 json 数据查询工具
 def_envs(){
-    . $dir_shell/api.sh
+    local token=$(cat $file_auth_user | jq -r .token)
     curl -s --noproxy "*" "http://0.0.0.0:5600/api/envs" -H "Authorization: Bearer $token" | jq .data
 }
 
 def_crons(){
-    . $dir_shell/api.sh
+    local token=$(cat $file_auth_user | jq -r .token)
     curl -s --noproxy "*" "http://0.0.0.0:5600/api/crons" -H "Authorization: Bearer $token" | jq .data
 }
 
@@ -27,7 +27,7 @@ ql_add_env_api() {
     local remarks=$3
     local url="http://0.0.0.0:5600/api/envs"
 
-    . $dir_shell/api.sh
+    local token=$(cat $file_auth_user | jq -r .token)
     if [[ $remarks ]]; then
         local api=$(
             curl -s --noproxy "*" "$url?t=$currentTimeStamp" \
@@ -35,7 +35,7 @@ ql_add_env_api() {
                 -H "Accept: application/json" \
                 -H "Authorization: Bearer $token" \
                 -H "Content-Type: application/json;charset=UTF-8" \
-                --data-raw "[{\"name\":\"$name\",\"value\":\"$value\",\"remarks\":\"$remarks\"}]"
+                --data-raw "[{\"name\":$name,\"value\":$value,\"remarks\":$remarks}]"
         )
     else
         local api=$(
@@ -44,7 +44,7 @@ ql_add_env_api() {
                 -H "Accept: application/json" \
                 -H "Authorization: Bearer $token" \
                 -H "Content-Type: application/json;charset=UTF-8" \
-                --data-raw "[{\"name\":\"$name\",\"value\":\"$value\"}]"
+                --data-raw "[{\"name\":$name,\"value\":$value}]"
         )
     fi
     code=$(echo $api | jq -r .code)
@@ -62,10 +62,11 @@ ql_add_cron_api() {
     local schedule=$1
     local command=$2
     local name=$3
-    local labels=$4
+    local url="http://0.0.0.0:5600/api/crons"
 
+    local token=$(cat $file_auth_user | jq -r .token)
     local api=$(
-        curl -s --noproxy "*" "http://0.0.0.0:5600/api/crons?t=$currentTimeStamp" \
+        curl -s --noproxy "*" "$url?t=$currentTimeStamp" \
             -H "Accept: application/json" \
             -H "Authorization: Bearer $token" \
             -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36" \
@@ -73,7 +74,7 @@ ql_add_cron_api() {
             -H "Origin: http://0.0.0.0:5700" \
             -H "Referer: http://0.0.0.0:5700/crontab" \
             -H "Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7" \
-            --data-raw "{\"name\":\"$name\",\"command\":\"$command\",\"schedule\":\"$schedule\",\"labels\":$labels}" \
+            --data-raw "{\"name\":${name},\"command\":${command},\"schedule\":${schedule}}" \
             --compressed
     )
     code=$(echo $api | jq -r .code)
@@ -90,12 +91,12 @@ ql_add_envs(){
     local i param name value remarks
     if [[ -f $dir_scripts/envs.json ]]; then
         echo -e "# 开始导入环境变量数据"
-        for ((i = 0; i <= 1000; i++)); do
+        for ((i = 0; i <= 10000; i++)); do
             for param in name value remarks; do
-                eval $param='$(jq -r .[$i].$param $dir_scripts/envs.json)'
-                eval echo \$$param
+                eval $param='$(jq .[$i].$param $dir_scripts/envs.json | grep -v null)'
+                [[ $(eval echo \$$param) ]] && eval echo \$$param
             done
-            [[ ! ${name} ]] && break
+            [[ ! ${name} || ${name} = null ]] && break
             ql_add_env_api "${name}" "${value}" "${remarks}"
         done
     else
@@ -105,15 +106,15 @@ ql_add_envs(){
 
 ## 导入添加定时任务
 ql_add_crons(){
-    local i param name command schedule labels
+    local i param name command schedule
     if [[ -f $dir_scripts/envs.json ]]; then
-        for ((i = 0; i <= 1000; i++)); do
-            for param in name command schedule labels; do
-                eval $param='$(jq -r .[$i].$param $dir_scripts/crons.json)'
-                eval echo \$$param
+        for ((i = 0; i <= 10000; i++)); do
+            for param in name command schedule; do
+                eval $param='$(jq -r .[$i].$param $dir_scripts/crons.json | grep -v null)'
+                [[ $(eval echo \$$param) ]] && eval echo \$$param
             done
-            [[ ! ${name} ]] && break
-            add_cron_api "${schedule}" "${command}" "${name}" "${labels}"
+            [[ ! ${name} || ${name} = null ]] && break
+            add_cron_api "${schedule}" "${command}" "${name}"
         done
     else
         echo -e "# 未发现 $dir_scripts/crons.json 文件，请检查后重试！"
@@ -138,12 +139,16 @@ main() {
     echo -e "# 当前版本：$Ver\n"
     case $1 in
         0)
-            echo -e "# 开始导出环境变量到 $dir_scripts/envs.json 文件"
+            echo -e "# 开始导出环境变量至 $dir_scripts/envs.json 文件"
+            [[ -f $dir_scripts/envs.json ]] && mv -f $dir_scripts/envs.json $dir_scripts/envs_bak.json
             def_envs > $dir_scripts/envs.json
+            [[ -f $dir_scripts/envs.json ]] && echo -e "# 已将环境变量导出至 $dir_scripts/envs.json 文件" || echo -e "# 环境变量导出失败，请稍后重试！"
             ;;
         1)
-            echo -e "# 开始导出定时任务到 $dir_scripts/crons.json 文件"
+            echo -e "# 开始导出定时任务至 $dir_scripts/crons.json 文件"
+            [[ -f $dir_scripts/crons.json ]] && mv -f $dir_scripts/crons.json $dir_scripts/crons_bak.json
             def_crons > $dir_scripts/crons.json
+            [[ -f $dir_scripts/crons.json ]] && echo -e "# 已将定时任务导出至 $dir_scripts/crons.json 文件" || echo -e "# 定时任务导出失败，请稍后重试！"
             ;;
         2)
             ql_add_envs
